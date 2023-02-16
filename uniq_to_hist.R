8a472998b591325c94439ecf98007470c582f457
Clark Fitzgerald <clarkfitzg@gmail.com>
2023-02-15 19:31:37 -0800

    .

--- uniq_to_hist.R
+++ uniq_to_hist.R
@@ -1,11 +1,19 @@
-
-
-hist_from_count = function(countfile, upper = 500, lower = -upper, varname = "temperature", ...)
+hist_from_count = function(countfile, upper = 140, lower = -60, varname = "temperature (degrees F)", ...)
 {
+    # Plot a histogram of the temperature for our weather data.
+
     d = read.table(countfile, row.names = NULL)
     colnames(d) = c("count", "value")
     d[["count"]] = as.numeric(d[["count"]])
+
+    # Convert from tenths of degree celsius to Fahrenheit
+    d[["value"]] = (d[["value"]]/10 * (9/5)) + 32
+
+    # Drop outliers
     d = d[lower < d[["value"]] & d[["value"]] < upper, ]
+
+    # general logic for creating a histogram from output of bash table:
+    #        sort | uniq --count > counts.txt
     result = hist(d[["value"]], plot = FALSE, ...)
     d[["bin"]] = cut(d[["value"]], breaks = result[["breaks"]])
     counts = tapply(d[["count"]], d[["bin"]], sum)
@@ -17,6 +25,7 @@ hist_from_count = function(countfile, upper = 500, lower = -upper, varname = "te
     result
 }
 
-h = hist_from_count("TMAX.txt")
-plot(h)
 
+# Example if you've saved resulting table as temp_counts.txt
+h = hist_from_count("temp_counts.txt")
+plot(h)
